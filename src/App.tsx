bf274a343c6e7ef5b357bd2744d3c8af4e992efc
m3rt7132 <mecetink@student.42kocaeli.com.tr>
2025-07-22 20:08:23 +0300

    refactor: A global edit for escaping the template

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+          <img
+            className="size-12 shrink-0"
+            src="/img/logo.svg"
+            alt="ChitChat Logo"
+          />
+          <div>
+            <div className="text-xl font-medium text-black dark:text-white">
+              ChitChat
+            </div>
+            <p className="text-gray-500 dark:text-gray-400">
+              You have a new message!
+            </p>
+          </div>
+        </div>
+        <h1 className="text-red-500 text-3xl font-bold">m3rt</h1>
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            asDAS {count}
+          </button>
+        </div>
       </div>
-	  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-	<p></p>
-      <h1 className='text-'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
