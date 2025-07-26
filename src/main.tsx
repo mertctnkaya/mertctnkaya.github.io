a7019724e6ae6e457427da5405632d85a2fc7626
m3rt7132 <mecetink@student.42kocaeli.com.tr>
2025-07-26 21:57:51 +0300

    refactor(i18n): Language provider is now helds the main react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { LanguageProvider } from "./i18n/LanguageProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<LanguageProvider>
+			<App />
+		</LanguageProvider>
+	</StrictMode>
+);
