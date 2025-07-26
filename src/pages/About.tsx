2f13299d04f5022fa9e6102cc446de590e709838
m3rt7132 <mecetink@student.42kocaeli.com.tr>
2025-07-26 21:59:10 +0300

    design(home): Home page is fully redesigned with theme of Dexter (red, dark)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,15 @@
+import translations from "../i18n/translates";
+import { useLanguage } from "../i18n/LanguageProvider";
+
 function About() {
-  return (
-	<div>
-	  <h1>About Us</h1>
-	  <p>This page contains information about our application.</p>
-	</div>
-  );
+	const { language } = useLanguage();
+	const t = translations[language].about;
+
+	return (
+		<div className="min-h-screen flex flex-col justify-center items-center bg-neutral-800 text-neutral-300 px-6 py-16 font-inter">
+			<h1>{t.title}</h1>
+			<p>{t.desc}</p>
+		</div>
+	);
 }
 export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,54 @@
-// import { Button } from "@/components/ui/button";
-import { DownloadIcon, Github, Linkedin } from "lucide-react";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { FiDownload } from "react-icons/fi";
+import { useLanguage } from "../i18n/LanguageProvider";
+import translations from "../i18n/translates";
 
 const Home = () => {
-  return (
-    <section className="bg-gradient-to-tr from-black via-red-950 to-red-600 min-h-screen font-inter flex flex-col justify-center items-center px-6 py-16 font-medium bg-neutral-800 text-neutral-300">
-      <div className="max-w-3xl w-full flex flex-col items-start gap-6 text-left">
-        <h1 className="text-4xl sm:text-5xl font-extrabold leading-tight tracking-tight">
-          Hi, I'm <span className="text-red-600 shadow-inherit text-shadow-xl ">Mertcan Çetinkaya</span>
-        </h1>
-        <p className="text-lg sm:text-xl text-neutral-400">
-          Software & CNC Enthusiast. Passionate about creating elegant technical
-          solutions with C, JS, and React. Currently studying at{" "}
-          <strong>42 Kocaeli</strong> and blending my knowledge of software with
-          CNC automation.
-        </p>
-        <div className="flex flex-wrap gap-4 mt-4">
-          <a
-            href="/cv.pdf"
-            download
-            className="inline-flex items-center gap-2 px-4 py-2 font-semibold rounded-md bg-red-600 text-white hover:bg-red-950 transition"
-          >
-            <DownloadIcon size={18} /> Download CV
-          </a>
-          <a
-            href="https://github.com/mertctnkaya/mertce"
-            target="_blank"
-            className="inline-flex items-center gap-2 px-4 py-2 border border-neutral-300 rounded-md  hover:bg-red-700 hover:border-red-900 transition"
-          >
-            <Github size={18} /> GitHub
-          </a>
-          <a
-            href="https://linkedin.com/in/mertcan-çetinkaya-80b850235"
-            target="_blank"
-            className="inline-flex items-center gap-2 px-4 py-2 border border-neutral-300 rounded-md hover:bg-red-700 hover:border-red-900 transition"
-          >
-            <Linkedin size={18} /> LinkedIn
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+	const { language } = useLanguage();
+	const t = translations[language].home;
+	return (
+		<section className="bg-linear-to-tr from-black via-red-950 to-red-600 min-h-screen font-inter flex flex-col justify-center items-center px-6 py-16 font-medium bg-neutral-800 text-neutral-300">
+			<img
+				src="/public/dexter.png"
+				alt="blood splash"
+				className="absolute top-10 left-[10%] w-[800px] h-[600px] opacity-30 blur-md pointer-events-none select-none object-cover"
+			/>
+			<div className="max-w-3xl w-full flex flex-col items-start gap-6 text-left">
+				<h1 className="text-4xl sm:text-5xl font-extrabold leading-tight tracking-tight">
+					{t.title} <span className="text-red-600 text-shadow-black text-shadow-lg">Mertcan Çetinkaya</span>
+				</h1>
+				<p className="text-lg sm:text-xl text-neutral-400">
+					{t.desc}
+					{/* Software & CNC Enthusiast. Passionate about creating elegant technical solutions with C, JS, and React. Currently
+					studying at <span className="text-white font-medium">42 Kocaeli</span> and blending my knowledge of software with
+					<span className="text-white font-medium"> CNC Lathe/Torna</span> programming and operating. */}
+				</p>
+				<div className="flex flex-wrap gap-4 mt-4">
+					<a
+						href="/cv.pdf"
+						download
+						className="inline-flex items-center gap-2 px-4 py-2 font-semibold rounded-md bg-red-700 text-white hover:bg-red-950 transition"
+					>
+						<FiDownload size={18} /> {t.downloadCv}
+					</a>
+					<a
+						href="https://github.com/mertctnkaya/mertce"
+						target="_blank"
+						className="inline-flex items-center gap-2 px-4 py-2 border border-neutral-300 rounded-md  hover:bg-red-700 hover:border-red-900 transition"
+					>
+						<BsGithub size={18} /> {t.github}
+					</a>
+					<a
+						href="https://linkedin.com/in/mertcan-çetinkaya-80b850235"
+						target="_blank"
+						className="inline-flex items-center gap-2 px-4 py-2 border border-neutral-300 rounded-md hover:bg-red-700 hover:border-red-900 transition"
+					>
+						<BsLinkedin size={18} /> {t.linkedin}
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
